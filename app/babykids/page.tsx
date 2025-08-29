5f8ac942ed9faab3d0e89135b0ed6397e00e66cb
rajeev21192-debug <rajeev21192@gmail.com>
2025-08-29 17:42:49 +0530

    v11

---
+++ app/babykids/page.tsx
@@ -0,0 +1,1 @@
+export default function Page() { return (<div><h1>Baby & Kids Page (Placeholder)</h1></div>); }
---
+++ app/momhood/page.tsx
@@ -0,0 +1,1 @@
+export default function Page() { return (<div><h1>Momhood Page (Placeholder)</h1></div>); }
